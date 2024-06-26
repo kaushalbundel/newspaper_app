9f61aa6bca85d8bffc8ef71a20b9aa7706a77bca
Kaushal Bundel <kaushalbundel@gmail.com>
2024-06-26 21:34:04 +0530

    Custom user model created

---
+++ users/forms.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+
+    '''
+    The purpose of this form is to access all the default fields + additional field age that we have added in the CustomUser model
+    '''
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('age',)
+
+class CustomUserChangeForm(UserChangeForm):
+
+    '''
+    The purpose of this class is to access all the fields present in the UserChangeForm class.
+    '''
+    class Meta(UserChangeForm):
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    '''
+The purpose of this model is so that we can have custom user model as per our requirement.
+    This model can be used in two places:
+    1. When the user is signing on or signing off
+    2. When the user is loggin in or logging off
+    3. When the super user is accessing the model from admin panel
+    '''
+    age = models.PositiveIntegerField(null=True, blank=True) #null indicates that the field can be null in the database while blank indicates that the field can be blank in the form
